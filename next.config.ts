93c3ed879cf8ca25f2f00ee14fb471e67823d5da
Sebastian <sebekmo01@gmail.com>
2026-03-14 21:12:01 +0100

    Translation fix

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  i18n: {
+    locales: ['en', 'pl'],
+    defaultLocale: 'en',
+    localeDetection: false,
+  },
 };
 
 export default nextConfig;
